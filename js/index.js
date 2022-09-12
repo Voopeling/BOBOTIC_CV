3a23244137691d1c320a9c282287008dd0ef95e8
Voopeling <voopeling@mail.ru>
2022-09-12 18:47:56 +0300

    add heder preview

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const time = document.querySelector('.preview__watch-time');
+function showTime() {
+    const options = {hour: 'numeric', minute: 'numeric',};
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString('ru-RU', options);
+    time.textContent = currentTime;
+    setTimeout(showTime, 1000);
+    showDate()
+}
+showTime();
+function showDate() {
+    const dateStr = document.querySelector('.preview__watch-date');
+    const weekdayStr = document.querySelector('.preview__watch-weekday');
+    const date = new Date();
+    const optionsMonth = {month: 'long',};
+    let month = date.toLocaleDateString('en-US', optionsMonth)
+    const optionsDay = {day: 'numeric',};
+    let day = date.toLocaleDateString('en-US', optionsDay)
+    const optionsWeekday = {weekday: 'long',};
+    let weekday = date.toLocaleDateString('en-US', optionsWeekday)
+    dateStr.textContent = `${month.substring(0,4)} ${day}`;
+    weekdayStr.textContent = `${weekday}`
+}
+
